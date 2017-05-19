a5f029750c4343b417557b865e1a7d04aee08fba
Tobias Käfer <tobias.kaefer@kit.edu>
2017-05-19 17:27:19 +0200

    two versions of div-by-3 for division of four and six

---
+++ runDiv-By-3_6.sh
@@ -0,0 +1,94 @@
+#!/bin/bash
+
+# Author: Tobias Käfer
+
+echo
+echo ============================================================
+echo === TURING MACHINE THAT CHECKS FOR DIVISIBILITY BY THREE ===
+echo ============================================================
+
+[[ $# -ne 2 ]] && { echo ; echo "Usage: <path-to-ldfu.sh> <uri-of-ldp-container>" ; exit 1 ; }
+
+LDF=$1
+LDPC=$2
+
+[[ ! "$LDPC" == */ ]] && { echo ; echo "The LDPC URI should end in a slash" ; exit 1 ; }
+
+if [ ! -f $LDF ]; then
+  echo
+  echo Linked Data-Fu executable not found
+  exit 1
+fi
+
+# Cleanup
+curl --fail -X DELETE $LDPC || { echo an error occurred related to the ldp container maintaining tape, state, and program ; exit 1 ; }
+
+echo
+echo "=== OVERWRITING data-retrieval.n3 FOR YOUR LDPC IMPLEMENTATION ==="
+
+# Creating data-retrieval.n3
+echo "@prefix http: <http://www.w3.org/2011/http#>." > data-retrieval.n3
+echo "@prefix http_m: <http://www.w3.org/2011/http-methods#>." >> data-retrieval.n3
+echo "@prefix ldp: <http://www.w3.org/ns/ldp#> ." >> data-retrieval.n3
+echo "{ _:h http:mthd http_m:GET ; http:requestURI <$LDPC> . }" >> data-retrieval.n3
+echo "{ <$LDPC> ldp:contains ?y . } => { [] http:mthd http_m:GET; http:requestURI ?y . } ." >> data-retrieval.n3
+
+set -x
+# Tape initialisation
+curl -X PUT "$LDPC"p01 --data-binary ' @prefix rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#> . <> rdf:first "#" ; rdf:rest <p02> . ' -Hcontent-type:text/turtle
+curl -X PUT "$LDPC"p02 --data-binary ' @prefix rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#> . <> rdf:first "#" ; rdf:rest <p03> . ' -Hcontent-type:text/turtle
+curl -X PUT "$LDPC"p03 --data-binary ' @prefix rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#> . <> rdf:first "1" ; rdf:rest <p04> . ' -Hcontent-type:text/turtle
+curl -X PUT "$LDPC"p04 --data-binary ' @prefix rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#> . <> rdf:first "1" ; rdf:rest <p05> . ' -Hcontent-type:text/turtle
+curl -X PUT "$LDPC"p05 --data-binary ' @prefix rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#> . <> rdf:first "0" ; rdf:rest <p06> . ' -Hcontent-type:text/turtle
+curl -X PUT "$LDPC"p06 --data-binary ' @prefix rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#> . <> rdf:first "#" ; rdf:rest <p07> . ' -Hcontent-type:text/turtle
+curl -X PUT "$LDPC"p07 --data-binary ' @prefix rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#> . <> rdf:first "#" ; rdf:rest <p08> . ' -Hcontent-type:text/turtle
+curl -X PUT "$LDPC"p08 --data-binary ' @prefix rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#> . <> rdf:first "#" ; rdf:rest <p09> . ' -Hcontent-type:text/turtle
+
+{ set +x; } 2>/dev/null
+
+echo
+echo === INITIALISING TURING MACHINE STATE ===
+
+set -x
+# State initialisieren
+curl -X PUT "$LDPC"tm1 --data-binary ' @prefix tm: <http://www.example.org/turing-machine#> . <> tm:hasInitialPosition <p03> ; tm:hasCurrentPosition <p03> ; tm:hasCurrentState <q0> .' -Hcontent-type:text/turtle
+{ set +x; } 2>/dev/null
+
+echo
+echo === INITIALISING TURING MACHINE TRANSITION FUNCTION ===
+
+set -x
+# Transition function initialisieren
+curl -X PUT "$LDPC"tr1 --data-binary @divisible-by-3.ttl -Hcontent-type:text/turtle
+{ set +x; } 2>/dev/null
+
+echo
+echo ========================================================================
+echo === INITIALISATION DONE. PERFORMING THE STEPS FOR THE TURING MACHINE ===
+echo ========================================================================
+
+# Loop the Turing machine's stepwise execution
+while true
+do
+
+echo
+echo === PERFORMING STEP ===
+# Performing the actual step
+$LDF -p data-retrieval.n3 -p tmrules.n3
+
+echo
+echo === CURRENT TURING MACHINE STATE ===
+# Outputting the current machine state
+$LDF -p data-retrieval.n3 -q tmquery.rq - 2>/dev/null
+
+echo
+echo === CURRENT TURING MACHINE TAPE STATE ===
+# Outputting the current tape state
+$LDF -p data-retrieval.n3 -q tapequery.rq - 2>/dev/null | (read -r; printf "%s\n" "$REPLY"; sort -u)
+
+echo
+
+read -n1 -rsp $'// Check Turing Machine state above.\n// If state is q<number>=calculation still in progress, otherwise you see acceptance or the remainder of the division.\n-----> Press any key to continue with the next step or Ctrl+C to exit...\n'
+
+done
+
